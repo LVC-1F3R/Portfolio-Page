cda58a17d36342937a22de21a252e47372e757bf
iskry <refocuslucifer@protonmail.com>
2023-03-11 15:15:26 -0600

    added some structure to the projects component. need fill out project templates

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,172 @@
 import React from 'react'
+import './projects.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
+
+
+
 
 const Projects = () => {
+
+
+
+  return (
+    <section id='experience'>
+      <h5>Check Out</h5>
+      <h2>My Recent Projects</h2>
+      
+      <div className="container experience__container">
+        <div className="projects">
+          <a href="https://github.com/username/repo">
+          <img src="project-image.jpg" alt="Project" />
+          </a>
+          <div className="project-title">Project Title</div>
+        </div>
+
+
+        <div className="projects">
+          <a href="https://github.com/username/repo">
+          <img src="project-image.jpg" alt="Project" />
+          </a>
+          <div className="project-title">Project Title</div>
+        </div>
+
+        <div className="projects">
+          <a href="https://github.com/username/repo">
+          <img src="project-image.jpg" alt="Project" />
+          </a>
+          <div className="project-title">Project Title</div>
+        </div>
+
+        <div className="projects">
+          <a href="https://github.com/username/repo">
+          <img src="project-image.jpg" alt="Project" />
+          </a>
+          <div className="project-title">Project Title</div>
+        </div>
+        
+
+
+
+      </div> 
+        <div className="container experience__container">
+        <div className="experience__frontend experience">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Tailwind</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+
+        <div className="experience__backend experience">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Express</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>AWS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>C#</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Apollo</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+                        <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>GraphQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+
+        {/* END OF BACKEND */}
+
+
+      </div>
+  </section>
+  )
 }
 
 export default Projects
