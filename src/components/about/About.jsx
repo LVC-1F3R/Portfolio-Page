8774f4b75e13315c75b5215bad8cd0461e180c57
iskry <refocuslucifer@protonmail.com>
2023-03-11 14:32:21 -0600

    added about section. added additional styling. throughout application

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import './about.css'
+import ME from '../../assets/img/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
-}
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            As a full-stack developer and AWS Cloud Practitioner certified, I'm always in the cloud and ready to deploy some serious code. I bring adaptability and problem-solving skills to every project, delivering bespoke solutions that meet client requirements. With a knack for technical communication and cross-functional collaboration, I excel at coordinating project-based efforts within tight deadlines.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default About
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import resume from '../../assets/resume.docx'
-
+import RESUME from '../../assets/resume.docx'
 
 const CTA = () => {
   return (
-  <div className='cta'>
-      <a href={resume} download>Download Resume</a>
-      <a href='#contact' className='btn btn--primary'>Let's Talk</a>
-  </div>
-)
+    <div className='cta'>
+        <a href={RESUME} download className='btn'>Download Resume</a>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
 }
 
 export default CTA
